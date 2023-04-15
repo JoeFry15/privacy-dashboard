f9d2a50ad59f691ee6d90f35ffb317847cc6be45
Joe Fry <103355865+OKPersonalComputer@users.noreply.github.com>
2023-04-15 10:48:22 +0100

    Add sample data test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { act, render } from '@testing-library/react';
 import App from './App';
 import { DeletionEmail } from './components/Deletion/DeletionEmail';
+import { EmailLeakChecker } from './components/Leaks/EmailLeakChecker';
+import { fetchSampleLeaksByEmail } from './clients/apiClient';
 
 test('renders Privacy Dashboard heading', () => {
   const { getByText } = render(<App />);
@@ -13,4 +15,9 @@ test('renders deletion email for Instabook', () => {
     const { getByText } = render(<DeletionEmail account="Instabook" />);
     const textElement = getByText(/Deletion Email for Instabook/i);
     expect(textElement).toBeInTheDocument();
+  });
+
+  test('the data is Adobe', async () => {
+    const data = await fetchSampleLeaksByEmail();
+    expect(data[0].Name).toMatch(/Adobe/i);
   });
