56ce4c822fdbb484072fdb02c50c84a01f827d5e
Joe Fry <103355865+OKPersonalComputer@users.noreply.github.com>
2023-06-26 11:34:47 +0100

    Add scam help page

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Home } from './components/Navigation/Home'
 import { Deletion } from './components/Deletion/Deletion'
 import { Access } from './components/Access/Access'
 import { Settings } from './components/Settings/Settings'
+import { ScamHelp } from './components/ScamHelp/ScamHelp'
 
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
         <Route path="/leaks" element={<EmailLeakChecker />} />
         <Route path="/deletion" element={<Deletion />} />
         <Route path="/access" element={<Access />} />
+        <Route path="/scam-help" element={<ScamHelp />} />
         <Route path="/settings" element={<Settings />} />
       </Routes>
     </BrowserRouter>
---
+++ src/components/ScamHelp/ScamHelp.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react"
+
+export function ScamHelp(): JSX.Element {
+
+
+    return <div>
+        <h1>Scam Help</h1>
+        <h4>Use this page if you have fallen victim to a scam</h4>
+        <p>APP Scam Info</p>
+        <p>Working with your bank</p>
+    </div>
+}
